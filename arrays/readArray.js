bf79cc149f87d944a3d61580305ae856aacb5fb9
Rico Rojas <djizco@yahoo.com>
2017-05-01 02:24:13 -0500

    Fix bug

--- arrays/readArray.js
+++ arrays/readArray.js
@@ -1,7 +1,8 @@
 // Use the filesystem to read and parse the json file 'array.json' and log it the console.
 const fs = require('fs');
 const path = require('path');
+
 const pathname = path.join(__dirname, 'array.json');
-const enconding = 'utf-8';
+const encoding = 'utf-8';
 
 // Your code here
