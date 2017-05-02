ce96031640b93ad84ee61c1d353b62e041b5e55a
Rico Rojas <djizco@yahoo.com>
2017-05-01 22:56:35 -0500

    Add problem to hello.js

--- hello/hello.js
+++ hello/hello.js
@@ -1,2 +1,10 @@
 // Log 'Hello World' to the console.
 console.log('Hello world!');
+
+// Log the sum of foo and bar to the console.
+const foo = 42;
+const bar = 11;
+
+const sum = foo + bar;
+
+console.log('Sum:', sum);
